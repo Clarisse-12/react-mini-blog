f4b0d1f2f15f67a3a69b19f6cbebca2bb010c2aa
Clarisse-12 <c.mukayiran@alustudent.com>
2026-09-22 12:26:49 +0200

    feat: add preview generation for post content in Post component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -5,6 +5,7 @@ interface PostProps {
 }
 
 function Post({ post }: PostProps) {
+    const preview = post.content.split("").slice(0,12).join(" ") + "...";
   return (
     <article>
       <h2>{post.title}</h2>
