b39d4a46b8634e27120a746982121c0737edee3e
Clarisse-12 <c.mukayiran@alustudent.com>
2026-09-22 12:11:02 +0200

    feat: add Post component to display blog post details

---
+++ src/components/Post/Post.tsx
@@ -0,0 +1,18 @@
+import type { Post as PostType } from '../../types/Post';
+
+interface PostProps {
+  post: PostType;
+}
+
+function Post({ post }: PostProps) {
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p>By {post.author}</p>
+      <p>{post.content}</p>
+      <small>{post.date}</small>
+    </article>
+  );
+}
+
+export default Post;
